69939ec1e0b0502f0491da488fa5fc9ad94dfa1c
nishat-jihad <alamnishat456@gmail.com>
2026-03-22 23:19:34 +0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,23 +6,23 @@ export const GROUPS = ['Science', 'Commerce', 'Arts'];
 
 export const HSC_SUBJECTS = {
   Commerce: [
-    'Bangla (1st Paper)', 'Bangla (2nd Paper)', 'English (1st Paper)', 'English (2nd Paper)',
-    'Information & Communication Technology (ICT)', 'Accounting (1st Paper)', 'Accounting (2nd Paper)',
-    'Economics (1st Paper)', 'Economics (2nd Paper)', 'Business Organization and Management (1st Paper)',
-    'Business Organization and Management (2nd Paper)', 'Finance, Banking and Insurance (1st Paper)',
-    'Finance, Banking and Insurance (2nd Paper)', 'Production, Management & Marketing (1st Paper)',
-    'Production, Management & Marketing (2nd Paper)', 'Statistics (1st Paper)', 'Statistics (2nd Paper)'
+    'Bangla 1st Paper', 'Bangla 2nd Paper', 'English 1st Paper', 'English 2nd Paper',
+    'Information & Communication Technology ICT', 'Accounting 1st Paper', 'Accounting 2nd Paper',
+    'Economics 1st Paper', 'Economics 2nd Paper', 'Business Organization and Management 1st Paper',
+    'Business Organization and Management 2nd Paper', 'Finance, Banking and Insurance 1st Paper',
+    'Finance, Banking and Insurance 2nd Paper', 'Production, Management & Marketing 1st Paper',
+    'Production, Management & Marketing 2nd Paper', 'Statistics 1st Paper', 'Statistics 2nd Paper'
   ],
   Arts: [
-    'Bangla (1st Paper)', 'Bangla (2nd Paper)', 'English (1st Paper)', 'English (2nd Paper)',
-    'Information & Communication Technology (ICT)', 'Economics', 'History', 'Islamic History and Culture',
+    'Bangla 1st Paper', 'Bangla 2nd Paper', 'English 1st Paper', 'English 2nd Paper',
+    'Information & Communication Technology ICT', 'Economics', 'History', 'Islamic History and Culture',
     'Civic and Good Governance', 'Sociology', 'Social Work', 'Geography', 'Logic'
   ],
   Science: [
-    'Bangla (1st Paper)', 'Bangla (2nd Paper)', 'English (1st Paper)', 'English (2nd Paper)',
-    'Physics (1st Paper)', 'Physics (2nd Paper)', 'Chemistry (1st Paper)', 'Chemistry (2nd Paper)',
-    'Biology (1st Paper)', 'Biology (2nd Paper)', 'Higher Mathematics (1st Paper)', 'Higher Mathematics (2nd Paper)',
-    'Information & Communication Technology (ICT)'
+    'Bangla 1st Paper', 'Bangla 2nd Paper', 'English 1st Paper', 'English 2nd Paper',
+    'Physics 1st Paper', 'Physics 2nd Paper', 'Chemistry 1st Paper', 'Chemistry 2nd Paper',
+    'Biology 1st Paper', 'Biology 2nd Paper', 'Higher Mathematics 1st Paper', 'Higher Mathematics 2nd Paper',
+    'Information & Communication Technology ICT'
   ]
 };
 
